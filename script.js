8a45d9f7a07f3c69320d3ce22edcb28bdda91f86
Priyansh Kandwal <153702794+priyansh-69@users.noreply.github.com>
2024-02-12 15:41:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const textEL = document.getElementById('text');
+const speakEL = document.getElementById('speak');
+
+speakEL.addEventListener('click', speakText);
+function speakText() {
+
+    // stop any speaking in progress
+    window.speechSynthesis.cancel();
+
+    const text = textEL.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+}
